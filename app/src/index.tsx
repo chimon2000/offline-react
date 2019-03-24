00fa89a0054d3b587f65b27f9dadb868a6635c73
Ryan Edge <ryan.c.edge@outlook.com>
2019-03-24 15:47:48 -0400

    Added global styles

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,35 +3,48 @@ import { render } from 'react-dom'
 import App from './App'
 import { Workbox } from 'workbox-window'
 import emitter from './emitter'
+import { Global, css } from '@emotion/core'
 
-render(<App />, document.getElementById('app'))
+render(
+  <>
+    <Global
+      styles={css`
+        body {
+          overflow-y: hidden;
+        }
+      `}
+    />
+    <App />
+  </>,
+  document.getElementById('app')
+)
 
 //1: Convert application to cache assets Workbox
 const registerServiceWorker = async () => {
-    try {
-        const wb = new Workbox('/sw.js')
+  try {
+    const wb = new Workbox('/sw.js')
 
-        wb.addEventListener('installed', event => {
-            if (event.isUpdate) {
-                console.log('emit cache-update')
-                emitter.emit('cache-update')
-            }
-        })
+    wb.addEventListener('installed', event => {
+      if (event.isUpdate) {
+        console.log('emit cache-update')
+        emitter.emit('cache-update')
+      }
+    })
 
-        wb.register()
+    wb.register()
 
-        console.log(`Service Worker registered! `)
+    console.log(`Service Worker registered! `)
 
-        return wb
-    } catch (err) {
-        console.log(`Service Worker registration failed: ${err}`)
+    return wb
+  } catch (err) {
+    console.log(`Service Worker registration failed: ${err}`)
 
-        return null
-    }
+    return null
+  }
 }
 
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', async () => {
-        await registerServiceWorker()
-    })
+  window.addEventListener('load', async () => {
+    // await registerServiceWorker()
+  })
 }
