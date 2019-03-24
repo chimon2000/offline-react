2192bc5bdd157afa14c3b17b8f6099a4e93c7930
Ryan Edge <ryan.c.edge@outlook.com>
2019-03-24 15:46:03 -0400

    Changed event form to modal.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,15 @@
 import React, { useState, useEffect } from 'react'
-import { ThemeProvider, Heading, Container, withToasts, Toast, css, ToastContainer } from 'fannypack'
+import {
+  ThemeProvider,
+  Heading,
+  Container,
+  withToasts,
+  Toast,
+  Modal,
+  DialogModal,
+  Button,
+  Pane
+} from 'fannypack'
 import { useFetch } from './hooks/use-fetch'
 import EventList from './components/EventList'
 import EventForm from './components/EventForm'
@@ -11,53 +21,76 @@ import { DbUpdateToast } from './components/DbUpdateToast'
 
 const baseUrl = '/api'
 
-export function App(props) {
-    const [eventsJson, loading] = useFetch<any[]>(`${baseUrl}/getAll`, true)
-    const [events, updateEvents] = useState([])
-    useEffect(() => {
-        updateEvents(eventsJson)
-    }, [eventsJson])
+export function App() {
+  const [eventsJson, loading] = useFetch<any[]>(`${baseUrl}/getAll`, true)
+  const [events, updateEvents] = useState([])
+  useEffect(() => {
+    updateEvents(eventsJson)
+  }, [eventsJson])
 
-    const addPostEvent = data => {
-        const newEvent = { id: Date.now(), ...data }
+  const addPostEvent = data => {
+    const newEvent = { id: Date.now(), ...data }
 
-        const body = JSON.stringify(newEvent)
-        const headers = new Headers({ 'Content-Type': 'application/json' })
-        const updatedEvents = events.concat(newEvent)
-        updateEvents(updatedEvents)
-        saveEventDataLocally([newEvent])
+    const body = JSON.stringify(newEvent)
+    const headers = new Headers({ 'Content-Type': 'application/json' })
+    const updatedEvents = events.concat(newEvent)
+    updateEvents(updatedEvents)
+    saveEventDataLocally([newEvent])
 
-        return fetch(`${baseUrl}/add`, {
-            method: 'POST',
-            headers: headers,
-            body: body
-        })
-    }
+    return fetch(`${baseUrl}/add`, {
+      method: 'POST',
+      headers: headers,
+      body: body
+    })
+  }
 
-    const updateEventsFromDb = async () => {
-        const events = await getLocalEventData()
-        console.log({ events })
-        updateEvents(events)
-    }
+  const updateEventsFromDb = async () => {
+    const events = await getLocalEventData()
+    console.log({ events })
+    updateEvents(events)
+  }
 
-    return (
-        <ThemeProvider>
-            <Container>
-                <Toast.Container>
-                    {toast => (
-                        <React.Fragment>
-                            <CacheUpdateToast toast={toast} />
-                            <DbUpdateToast toast={toast} onClick={updateEventsFromDb} />
-                        </React.Fragment>
-                    )}
-                </Toast.Container>
-                <Heading>Current Events {!loading && `(${events.length})`}</Heading>
-                {loading ? <div>Loading...</div> : <EventList events={events} />}
-                <EventForm onSubmit={addPostEvent} />
-            </Container>
-        </ThemeProvider>
-    )
+  return (
+    <ThemeProvider>
+      <Container>
+        <Toast.Container>
+          {toast => (
+            <React.Fragment>
+              <CacheUpdateToast toast={toast} />
+              <DbUpdateToast toast={toast} onClick={updateEventsFromDb} />
+            </React.Fragment>
+          )}
+        </Toast.Container>
+        <Heading>Current Events {!loading && `(${events.length})`}</Heading>
+        {renderEventFormModal({ addPostEvent })}
+        {loading ? <div>Loading...</div> : <EventList events={events} />}
+      </Container>
+    </ThemeProvider>
+  )
 }
+
+const renderEventFormModal = ({ addPostEvent }) => (
+  <Modal.Container>
+    {(modal: any) => (
+      <>
+        <Pane paddingBottom="1rem">
+          <Button use={Modal.Show} {...modal}>
+            Add Event
+          </Button>
+        </Pane>
+        <DialogModal title="Add an event" hideOnClickOutside={false} {...modal}>
+          <EventForm
+            onSubmit={event => {
+              addPostEvent(event)
+              modal.hide()
+            }}
+            onReset={modal.hide}
+          />
+        </DialogModal>
+      </>
+    )}
+  </Modal.Container>
+)
 
 export default hot(module)(withToasts(App))
 //
