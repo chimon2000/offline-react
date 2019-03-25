2274ea90626859ce660089c9912822d78b093862
Ryan Edge <ryan.c.edge@outlook.com>
2019-03-24 22:44:06 -0400

    Fixed toasts

--- app/src/components/CacheUpdateToast.tsx
+++ app/src/components/CacheUpdateToast.tsx
@@ -3,24 +3,24 @@ import React, { useEffect } from 'react'
 import emitter from '../emitter'
 
 export function CacheUpdateToast({ toast }) {
-    useEffect(() => {
-        console.log('add cache update listener!')
+  useEffect(() => {
+    console.log('add cache update listener!')
 
-        function handleCacheUpdate() {
-            toast.info({
-                message: (
-                    <Text cursor="pointer" onClick={() => window.location.reload()}>
-                        A new version of the app is available. <b>tap here</b> to refresh
-                    </Text>
-                ),
-                autoDismissTimeout: 0
-            })
-        }
+    function handleCacheUpdate() {
+      toast.info({
+        message: (
+          <Text cursor="pointer" onClick={() => window.location.reload()}>
+            A new version of the app is available. <b>tap here</b> to refresh
+          </Text>
+        ),
+        autoDismissTimeout: 0
+      })
+    }
 
-        emitter.on('cache-update', handleCacheUpdate)
+    emitter.on('cache-update', handleCacheUpdate)
 
-        // return emitter.off('cache-update', handleCacheUpdate)
-    }, [])
+    return () => emitter.off('cache-update', handleCacheUpdate)
+  }, [])
 
-    return null
+  return null
 }
--- app/src/components/DbUpdateToast.tsx
+++ app/src/components/DbUpdateToast.tsx
@@ -3,30 +3,30 @@ import React, { useEffect } from 'react'
 import emitter from '../emitter'
 
 type Props = {
-    toast: {
-        info: (props) => void
-    }
-    onClick: () => void
+  toast: {
+    info: (props) => void
+  }
+  onClick: () => void
 }
 
 export function DbUpdateToast({ toast, onClick }: Props) {
-    useEffect(() => {
-        function handleCacheUpdate() {
-            //FIXME: Cannot programmatically close toasts
-            toast.info({
-                message: (
-                    <Text cursor="pointer" onClick={onClick}>
-                        Some new events have been created. <b>tap here</b> to refresh
-                    </Text>
-                ),
-                autoDismissTimeout: 8000
-            })
-        }
+  useEffect(() => {
+    function handleCacheUpdate() {
+      //FIXME: Cannot programmatically close toasts
+      toast.info({
+        message: (
+          <Text cursor="pointer" onClick={onClick}>
+            Some new events have been created. <b>tap here</b> to refresh
+          </Text>
+        ),
+        autoDismissTimeout: 8000
+      })
+    }
 
-        emitter.on('db-update', handleCacheUpdate)
+    emitter.on('db-update', handleCacheUpdate)
 
-        // return emitter.off('db-update', handleCacheUpdate)
-    }, [])
+    return () => emitter.off('db-update', handleCacheUpdate)
+  }, [])
 
-    return null
+  return null
 }
